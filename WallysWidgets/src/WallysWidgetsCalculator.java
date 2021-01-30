480153ca10186352ca769f8dde33793f645d5ec0
misch4777 <58167452+misch4777@users.noreply.github.com>
2021-01-30 21:31:08 +0000

    Update WallysWidgetsCalculator.java

--- WallysWidgets/src/WallysWidgetsCalculator.java
+++ WallysWidgets/src/WallysWidgetsCalculator.java
@@ -5,26 +5,23 @@ public class WallysWidgetsCalculator {
     public static HashMap<Integer,Integer> getPacks(int widgetsRequired,int[] packSizes){
         HashMap<Integer,Integer> output = new HashMap<Integer,Integer>();
         int[] packageNums = new int[packSizes.length];
-        int runTotal;
         Arrays.sort(packSizes);
 
         for(int i=packSizes.length-1;i>0;i--){
-            runTotal=0;
-            for(int j=0;j<i;j++){
-                runTotal=runTotal+packSizes[j];
+            while(widgetsRequired>=packSizes[i]){
+                widgetsRequired=widgetsRequired-packSizes[i];
+                packageNums[i]++;
             }
-            while(widgetsRequired>packSizes[i-1]){
-                if((runTotal>=widgetsRequired)&&(runTotal<packSizes[i])){
-                    widgetsRequired=widgetsRequired-packSizes[i-1];
-                    packageNums[i-1]++;
-                } else {
-                    widgetsRequired=widgetsRequired-packSizes[i];
-                    packageNums[i]++;
-                }
+            if((widgetsRequired>packSizes[i-1])&&(packSizes[i]-widgetsRequired<packSizes[0])){
+                widgetsRequired=widgetsRequired-packSizes[i];
+                packageNums[i]++;
             }
         }
-        if(widgetsRequired>=0){
-            packageNums[0]++;
+        if(widgetsRequired>0){
+            while(widgetsRequired>0){
+                widgetsRequired=widgetsRequired-packSizes[0];
+                packageNums[0]++;
+            }
         }
 
         for(int i=0;i<packSizes.length;i++){
@@ -32,7 +29,6 @@ public class WallysWidgetsCalculator {
                 output.put(packSizes[i],packageNums[i]);
             }
         }
-
         return output;
     }
 
@@ -43,5 +39,4 @@ public class WallysWidgetsCalculator {
         int[] b = new int[]{250, 500, 1000, 2000, 5000};
         System.out.println("Package sizes to be shipped as as follows: \n" + getPacks(a, b));
     }
-
 }
